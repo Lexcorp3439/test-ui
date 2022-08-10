221bfb9a66ffb156eb511a4022e8283033313e08
Romanov Aleksey Leonidovich <romanovaleks@ozon.ru>
2022-08-10 18:54:09 +0300

    Updated test_search_input and test_donation_button

--- steps/main_page_steps.py
+++ steps/main_page_steps.py
@@ -14,6 +14,15 @@ class MainPageSteps(BaseSteps):
         assert self.main_page.search_input.get_property("value") == search_text, "Описание ошибки"
         self.main_page.start_search.click()
 
+    def open_donate_page(self):
+        assert self.main_page.donate_button.text == "Donate", "Donation button text is not Donate"
+        self.main_page.donate_button.click()
+
+    # TODO
     def check_search_result(self):
+        pass
+
+    # TODO: этот метод должен лежать в отдельном step и с отдельной page
+    def check_donate_page_is_open(self):
         pass
 
--- tests/test_python_org_page.py
+++ tests/test_python_org_page.py
@@ -4,18 +4,13 @@ from selenium import webdriver
 import steps
 
 
-# def test_donation_button():
-#     driver = open_python_page()
-#     main_page = MainPage(driver)
-#
-#     assert main_page.donate_button.text == "Donate", "Donation button text is not Donate"
-#     main_page.donate_button.click()
-#     assert main_page.donation_title.is_displayed(), "Donation title not visible"
-#     assert main_page.donation_title.text == "Donation for the PSF", "Donation title text is not valid"
+# FIXME
+def test_donation_button():
+    driver = steps.driver_steps().create_driver_and_open_python_page()
+    steps.main_page(driver).open_donate_page()
 
 
 def test_search_input():
-    search_text = "hello"
-
     driver = steps.driver_steps().create_driver_and_open_python_page()
-    steps.main_page(driver).search(search_text=search_text)
+    steps.main_page(driver).search(search_text="hello")
+    steps.main_page(driver).check_search_result()
